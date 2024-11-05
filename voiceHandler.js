d6592d1cfaa614002ddfc0bfb2a9d7d0f2521c3f
Olzhas <mrbll@list.ru>
2024-11-05 10:15:44 +0500

    feat: add week

--- voiceHandler.js
+++ voiceHandler.js
@@ -37,7 +37,10 @@ const handleVoiceMessage = async (bot, msg) => {
                 // Отправляем сообщение об успешной загрузке и тексте
                 await bot.sendMessage(chatId, 'Ваше голосовое сообщение: ' + audioText);
 
-                const plan = await generatePlan(audioText, username, bot, chatId)
+                const plan = await generatePlan(audioText, username, bot, chatId);
+
+                console.log("Всё отлично!");
+                
 
                 await bot.sendMessage(chatId, plan);
 
